4640b42adfb1b0065690794daf5faa3bde261f45
Aestriex <68126194+aestriex@users.noreply.github.com>
2022-04-14 13:16:15 -0700

    resource

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,34 @@
-"use strict";
-import Discord from "discord.js";
-import { token } from "../config.json";
+import fs from 'node:fs';
+import { Client, Collection, Intents } from 'discord.js';
+import { token } from './config.json';
 
-export var client = new Discord.Client({
-    intents: 3067,
-    commands: null,
-    buttons: null,
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.ts'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-["Commands", "Events", "Buttons"].forEach((handler) => {
-    require(`./${handler}`)(client, PG);
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
-client.login(token)
+client.login(token);
